ab30c64eab06c1a7d2dc9725e8bf094f040f27b3
Yegor Vialov <vyalov.egor@gmail.com>
2018-11-04 23:20:58 +0200

    v.0.3.7

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ part 'ui_widgets/card_header_widget.dart';
 
 EventBus eventBus = new EventBus();
 const String appName = "HA Client";
-const appVersion = "0.3.6";
+const appVersion = "0.3.7";
 
 String homeAssistantWebHost;
 
