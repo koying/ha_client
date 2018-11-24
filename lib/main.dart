36410752e40c2a0f9025ee9b97a70d79f87b3448
Yegor Vialov <vyalov.egor@gmail.com>
2018-11-24 17:29:33 +0200

    Fix app version

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,7 @@ part 'ui_widgets/card_header_widget.dart';
 
 EventBus eventBus = new EventBus();
 const String appName = "HA Client";
-const appVersion = "0.3.10-73";
+const appVersion = "0.3.10";
 
 String homeAssistantWebHost;
 
