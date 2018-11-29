4bece42693f617c08a3c9067d7b8cc31a5a4488d
Yegor Vialov <vyalov.egor@gmail.com>
2018-11-29 21:46:47 +0200

    build number

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ part 'ui_widgets/card_header_widget.dart';
 
 EventBus eventBus = new EventBus();
 const String appName = "HA Client";
-const appVersion = "0.3.10-75";
+const appVersion = "0.3.10-76";
 
 String homeAssistantWebHost;
 
