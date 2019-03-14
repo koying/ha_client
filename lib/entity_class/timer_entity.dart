125f3ac16cf048d98d03976c35851b5b6b676f2b
estevez-dev <vyalov.egor@gmail.com>
2019-03-14 16:39:37 +0200

    Resolves #327 Timer duration parsing error

--- lib/entity_class/timer_entity.dart
+++ lib/entity_class/timer_entity.dart
@@ -9,22 +9,31 @@ class TimerEntity extends Entity {
   void update(Map rawData) {
     super.update(rawData);
     String durationSource = "${attributes["duration"]}";
-    List<String> durationList = durationSource.split(":");
-    if (durationList.length == 1) {
-      duration = Duration(seconds: int.tryParse(durationList[0] ?? 0));
-    } else if (durationList.length == 2) {
-      duration = Duration(
-          hours: int.tryParse(durationList[0]) ?? 0,
-          minutes: int.tryParse(durationList[1]) ?? 0
-      );
-    } else if (durationList.length == 3) {
-      duration = Duration(
-          hours: int.tryParse(durationList[0]) ?? 0,
-          minutes: int.tryParse(durationList[1]) ?? 0,
-        seconds: int.tryParse(durationList[2]) ?? 0
-      );
+    if (durationSource != null && durationSource.isNotEmpty) {
+      try {
+        List<String> durationList = durationSource.split(":");
+        if (durationList.length == 1) {
+          duration = Duration(seconds: int.tryParse(durationList[0] ?? 0));
+        } else if (durationList.length == 2) {
+          duration = Duration(
+              hours: int.tryParse(durationList[0]) ?? 0,
+              minutes: int.tryParse(durationList[1]) ?? 0
+          );
+        } else if (durationList.length == 3) {
+          duration = Duration(
+              hours: int.tryParse(durationList[0]) ?? 0,
+              minutes: int.tryParse(durationList[1]) ?? 0,
+              seconds: int.tryParse(durationList[2]) ?? 0
+          );
+        } else {
+          Logger.e("Strange $entityId duration format: $durationSource");
+          duration = Duration(seconds: 0);
+        }
+      } catch (e) {
+        Logger.e("Error parsing duration for $entityId: ${e.toString()}");
+        duration = Duration(seconds: 0);
+      }
     } else {
-      Logger.e("Cann't parse $entityId duration: $durationSource");
       duration = Duration(seconds: 0);
     }
   }
