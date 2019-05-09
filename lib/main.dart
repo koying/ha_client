d1c9fddba60fc1cc5949497062e74bcd74be3243
estevez-dev <vyalov.egor@gmail.com>
2019-05-09 13:17:30 +0300

    v. 0.5.4

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,7 @@ part 'ui_widgets/config_panel_widget.dart';
 
 EventBus eventBus = new EventBus();
 const String appName = "HA Client";
-const appVersion = "0.5.3";
+const appVersion = "0.5.4";
 
 String homeAssistantWebHost;
 
